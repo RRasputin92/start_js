1371ebe615acadf06afc4821742a82c7b078b5d6
Roman <roma.war@inbox.ru>
2019-08-01 18:30:00 +0300

    первый комит

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+
+
+//var leftBorderWidth = 15;
+//console.log(leftBorderWidth);
+
+//var number = 5;
+//var string = "Hello";
+//var sym = Symbol();
+//var boolean = true;
+//null;
+//undefined;
+//var obj = {};
+
+//alert('Привет Роман');
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+let answer = prompt("Вам есть 18?", "да");
+
+console.log(typeof(answer));
+
+
